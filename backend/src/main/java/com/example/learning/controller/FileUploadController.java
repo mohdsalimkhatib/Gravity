ab0f8593ddd9f4fc4b6b3ccd685e5715ff1a6bc7
mohammedsaleemkhatib <mohdsalimkhatib@yahoo.com>
2025-12-14 16:39:06 -0500

    setup reverse proxy

--- backend/src/main/java/com/example/learning/controller/FileUploadController.java
+++ backend/src/main/java/com/example/learning/controller/FileUploadController.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/upload")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:3000")
 public class FileUploadController {
 
     private final Path fileStorageLocation;
